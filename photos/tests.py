5bd51be64760f4a057c4cd074eb757e3b2529000
martyminion <martkimwaweru@gmail.com>
2020-05-22 20:26:30 +0300

    ft:Tests on the first two methods to confirm if tests are working

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from .models import Location,Tags,Category,Image
 
 # Create your tests here.
+
+class ImageTest(TestCase):
+  '''
+  This class defines the test cases for the Image model
+  '''
+  def setUp(self):
+    '''
+    This function runs at the beginning of every test
+    '''
+    #image
+    new_image = Image(image_url = "path/to/imag", name = "Dubai Mall", description = "A lively night out",
+        locations = "Dubai", categories = "Travel", tags = "food")
+    #tags
+    new_tag = Tags(name = "food")
+    #categories
+    new_category = Category(name = "Travel")
+    #location
+    new_location = Location(city = "Dubai", country = "UAE", specific_locale = "Dubai Mall Waterfront")
+
+    new_location.save()
+    new_category.save()
+    new_tag.save()
+    new_image.save()
+
+  '''
+  Tests
+  '''
+
+  def test_instance(self):
+    '''
+    Test to see if instantiation was correct
+    '''
+    self.assertTrue(isinstance(self.new_image,Image))
+
+  def test_get_image_by_id(self,id):
+    '''
+    Test to check if function can return correct data
+    '''
+    image = Image.get_image_by_id(1)
+    self.assertEqual(image,self.new_image)
+
