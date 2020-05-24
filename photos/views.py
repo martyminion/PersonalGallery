4a3b73be315b48de3cddebbd200c76f87c2e5038
martyminion <martkimwaweru@gmail.com>
2020-05-24 19:05:41 +0300

    this commit adds logic to prevent the app from crashing due to user input in the search bar

--- photos/views.py
+++ photos/views.py
@@ -17,16 +17,24 @@ def search_tags(request):
   '''
   this will return search results based on the tags
   '''
+  title = "search results"
   if 'searchtag' in request.GET and request.GET["searchtag"]:
     search_tag = request.GET.get("searchtag")
     tags = Tags.objects.all()
     for tag in tags:
-      if tag.name == search_tag:
+      if tag.name==search_tag:
         search_results = Image.search_by_tags(tag)
-        title = "search results"
-        return render(request,'search_results.html',{"title":title,"search_results":search_results,"search_term":search_tag})
+        if len(search_results) > 0:
+          return render(request,'search_results.html',{"title":title,"search_results":search_results,"search_term":search_tag})
+        else:
+          message_noimage = "There is no image with that tag"
+          return render(request,'search_results.html',{"message":message_noimage,"title":title,"search_term":search_tag})
+
+      else:
+        message_notag = "There is no tag with that name" 
+    else:
+      return render(request,'search_results.html',{"message":message_notag,"title":title})
   else:
-    title = "search results"
     message = "You haven't searched for any images"
     return render(request,'search_results.html',{"message":message,"title":title})  
   
