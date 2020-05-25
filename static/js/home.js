af5e1329196442fc2bd34d92e5fccd3111e680ac
martyminion <martkimwaweru@gmail.com>
2020-05-25 03:57:36 +0300

    Adds modal using jquery

--- static/js/home.js
+++ static/js/home.js
@@ -1,8 +1,16 @@
-$(document).ready(function()
-{
+function showModal(imgname,imgdesc,imgurl,imglocsp,imglocct,imgloccntry,imgcat){
+  $("#imgmdtitle").text(imgname)
+  $("#ScrollableModal").modal("show")
+  $(".modal-title").text(imgname)
+  $(".mod-img").attr("src",imgurl)
+  $("#imgmddescription").text(imgdesc)
+  $("#imgmdlocationspecifc").text("Location: " + imglocsp)
+  $("#imgmdlocation").text(imglocct+","+imgloccntry)
+  $("#imgmdcategory").text("Category: " + imgcat)
+}
 $("#copyimg").click(function(){
   alert('image copied to clipboard')
 })
-})
+
 
 
