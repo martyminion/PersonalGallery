26edeef7dc66e8031242a3dc653418bd6e8250d6
martyminion <martkimwaweru@gmail.com>
2020-05-25 07:46:49 +0300

    ch:deployment to heroku4

--- gallery/settings.py
+++ gallery/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',default=false, cast=bool)
+DEBUG = config('DEBUG',default=False, cast=bool)
 MODE = config('MODE',default='dev')
 ALLOWED_HOSTS = [config('ALLOWED_HOSTS',cast=Csv)]
 
