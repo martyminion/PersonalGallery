cc10f62b03d10a2639e3800533149f07a9fb4736
martyminion <martkimwaweru@gmail.com>
2020-05-25 16:18:51 +0300

    ft:The use of masonry js to create a grid for the images

--- static/js/home.js
+++ static/js/home.js
@@ -9,11 +9,23 @@ function showModal(imgname,imgdesc,imgurl,imglocsp,imglocct,imgloccntry,imgcat){
   $("#imgmdcategory").text("Category: " + imgcat)
 }
 
-
-$(document).ready(function()
-{
-  $("#copyimg").click(function(){
+function copy(){
+    $("#copy_image_url").select()
+    document.execCommand('copy')
     alert('image copied to clipboard')
-  })
+}
 
-  });
+var elem = document.querySelector('.grid');
+var msnry = new Masonry( elem, {
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});
+
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry( '.grid', {
+  // options
+});
+
+  
