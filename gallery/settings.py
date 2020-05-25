b4f0e8caa7e524dcda5213d29aa7179837931e06
martyminion <martkimwaweru@gmail.com>
2020-05-25 07:43:24 +0300

    ch:deployment to heroku3

--- gallery/settings.py
+++ gallery/settings.py
@@ -96,7 +96,7 @@ else:
         )
     }
 db_from_env = dj_database_url.config(conn_max_age=5000)
-DATABASES['default'].update()db_from_env
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
